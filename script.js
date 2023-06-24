275992ff6e0eea98a2cad4c33a627dd2a40840eb
MrAayushGupta <teamaayushgupta@gmail.com>
2023-06-24 17:52:21 +0530

    all changes for today

---
+++ script.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".multiText",{
+    strings : ["Coder","Designer","Youtuber","Fashion"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 120,
+    backDelay : 1800
+})
